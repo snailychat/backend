8bbd9e7e5749853a63bf7c7d8f8008298a99ca34
Mafia#7777 <67177667+Mafia-7777@users.noreply.github.com>
2021-08-12 22:07:22 -0400

    Works...

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const ws = require('ws');
+const randomatic = require('randomatic');
 const port = process.env.SERVER_PORT || process.env.PORT || 8080;
 
 const wss = new ws.Server({
 	port
 });
+
+let connections = [];
 wss.on('connection', socket => {
-	(async () => {
-		await new Promise(res => {
-			socket.once('message', (msg) => {
-				console.log(msg)
-				res();
-			});
+	socket.__id = randomatic('aA0', 25);
+	connections.push(socket)
+	socket.on('message', msg => {
+		const { content, username } = JSON.parse(msg);
+		console.log(content, username)
+		connections.map(x => {
+			if(x.__id == socket.__id) return;
+			x.send(JSON.stringify({
+				content, username 
+			}));
 		})
-		
-	})();
+	});
+	socket.once('close', () => {
+		connections = connections.filter(x => x.__id != socket.__id);
+	});
 });
